adc47bc91d6c0192ba6c3bb794b24e0453961e73
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2022-11-06 23:00:20 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from OpenGL import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+    
+    
